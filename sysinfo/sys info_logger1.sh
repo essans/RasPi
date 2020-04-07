6f08fcfacfe64b61bfd254e8b7d2b4789c215556
essans <30513008+essans@users.noreply.github.com>
2020-04-06 23:17:05 -0400

    Create sys info_logger1.sh

---
+++ sysinfo/sys info_logger1.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+#sysinfo_attr=$(/home/pi/code/bash/sysinfo.sh | awk '{print $1}' | tr -d "=" | sed 's/SYSTEM_INFORMATION//g' | sed 's/   //g')
+
+sysinfo=$(/home/pi/code/bash/sysinfo.sh)
+
+sysinfo_attr=$(echo "$sysinfo" | awk '{print $1}' | tr -d "=" | sed 's/SYSTEM_INFORMATION//g' | sed 's/   //g')
+
+
+#echo $sysinfo
+
+attributes=$(echo $sysinfo_attr)
+
+headertosave=""
+recordtosave=""
+
+
+sep=","
+
+for attr in $attributes
+do
+        headertosave+="${attr},"
+
+        value=$(echo "$sysinfo" | awk "/(^$attr)/" | awk '{$1="";print $0}')
+
+
+        recordtosave+="${value},"
+
+done
+
+echo "$headertosave"
+
+echo "$recordtosave"
