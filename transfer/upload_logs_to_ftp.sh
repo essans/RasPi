eab3f0a91b10a1b755353526a868202e23ab12c4
essans <30513008+essans@users.noreply.github.com>
2020-04-10 15:37:16 -0400

    Update upload_logs_to_ftp.sh

--- transfer/upload_logs_to_ftp.sh
+++ transfer/upload_logs_to_ftp.sh
@@ -1,30 +1,37 @@
 #!/bin/bash
 
-#uploads changes to logs to ftp
-#runs via cron:
-#2,17,32,47 * * * * bash /home/pi/code/cron/upload_logs_to_ftp.sh
-
-
-HOST='ftp://ftp.raspi.soobratty.com'
-USER='user'
-PASSWORD='password'
+HOST='raspi.soobratty.com'
+USER='raspi'
+PASSWORD='vYah%(RZCHE!&cbpSwDD'
 
 cd /home/pi/log
 
-prev_sysinfo_log = `cat /home/pi/log/sysinfo.ftp`
-current_sysinfo_log = `ls -ltr /home/pi/log/sysinfo.log`
+prev_sysinfo_dump=`cat /home/pi/log/sysinfodump.ftp`
+current_sysinfo_dump=`ls -ltr /home/pi/log/sysinfo.dump`
 
-prev_access_log = `cat /home/pi/log/accesslog.ftp`
-current_access_log = `ls -ltr /home/pi/log/access.log`
+prev_sysinfo_log=`cat /home/pi/log/sysinfo.ftp`
+current_sysinfo_log=`ls -ltr /home/pi/log/sysinfo.log`
+
+prev_access_log=`cat /home/pi/log/accesslog.ftp`
+current_access_log=`ls -ltr /home/pi/log/access.log`
+
+
+
+if [ "$prev_sysinfo_dump"  != "$current_sysinfo_dump" ]; then
+        curl -T /home/pi/log/sysinfo.dump -u $USER:$PASSWORD ftp://ftp.raspi.soobratty.com/
+        ls -ltr /home/pi/log/sysinfo.dump > sysinfodump.ftp
+fi
+
 
 
 if [ "$prev_sysinfo_log"  != "$current_sysinfo_log" ]; then
-	curl -T /home/pi/log/sysinfo.log -u $USER:$PASSWORD $HOST
+	curl -T /home/pi/log/sysinfo.log -u $USER:$PASSWORD ftp://ftp.raspi.soobratty.com/
 	ls -ltr /home/pi/log/sysinfo.log > sysinfo.ftp
 fi
 
 
+
 if [ "$prev_access_log" != "$current_access_log" ]; then
-	curl -T /home/pi/log/access.log -u $USER:$PASSWORD $HOST
+	curl -T /home/pi/log/access.log -u $USER:$PASSWORD ftp://ftp.raspi.soobratty.com
 	ls -ltr /home/pi/log/access.log > accesslog.ftp
 fi
