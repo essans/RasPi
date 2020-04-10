09d9630a02bfc806f81f73700b3274dae479e21e
essans <30513008+essans@users.noreply.github.com>
2020-04-10 15:36:35 -0400

    Update sys info_logger.sh

--- sysinfo/sys info_logger.sh
+++ sysinfo/sys info_logger.sh
@@ -1,9 +1,10 @@
-#!/bin/bash 
+#!/bin/bash
 
 #Execute via crontab -e
 #0,15,30,45 * * * * bash /home/pi/code/bash/sysinfo_logger.sh >> /home/pi/log/sysinfo.log
 
-sysinfo_attr=$(/home/pi/code/bash/sysinfo.sh | awk '{print $1}' | tr -d "=" | sed 's/SYSTEM_INFORMATION//g' | sed 's/   //g')
+
+#sysinfo_attr=$(/home/pi/code/bash/sysinfo.sh | awk '{print $1}' | tr -d "=" | sed 's/SYSTEM_INFORMATION//g' | sed 's/   //g')
 
 sysinfo=$(/home/pi/code/bash/sysinfo.sh)
 
@@ -17,22 +18,26 @@ attributes=$(echo $sysinfo_attr)
 headertosave=""
 recordtosave=""
 
-
-sep=","
+quote="\""
 
 for attr in $attributes
 do
-        headertosave+="${attr},"
-
-        value=$(echo "$sysinfo" | awk "/(^$attr)/" | awk '{$1="";print $0}')
+	headertosave+="${attr},"
 
 
-        recordtosave+="${value},"
+	value=$(echo "$sysinfo" | awk "/(^$attr)/" | awk '{$1="";print $0}')
+
+	value=${value:1}
+
+	kv_pair="$quote${attr}$quote:$quote${value}$quote"
+
+	recordtosave+="${kv_pair}, "
+
 
 done
 
 headertosave=${headertosave::-1}
 
-recordtosave=${recordtosave::-1}
+recordtosave=${recordtosave::-2}
 
-echo $recordtosave
+echo "{$recordtosave}" 
