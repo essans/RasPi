22547d8413b551651e680b36afce66ce77007e61
essans <30513008+essans@users.noreply.github.com>
2020-04-11 11:41:32 -0400

    Update upload_logs_to_ftp.sh

--- transfer/upload_logs_to_ftp.sh
+++ transfer/upload_logs_to_ftp.sh
@@ -2,15 +2,14 @@
 
 HOST='raspi.soobratty.com'
 USER='raspi'
-PASSWORD='N)RBtv{Qh@B8'
+PASSWORD=''
 
 uname=`hostname -s`
 
 cd /home/pi/log
 
 prev_sysinfo_dump=`cat /home/pi/log/$uname"_sysinfodump.ftp"`
-
-current_sysinfo_dump=`ls -ltr /home/pi/log/$uname"_sysinfo.dump"`
+current_sysinfo_dump=`ls -ltr /home/pi/log/$uname"_sysinfodump.log"`
 
 prev_sysinfo_log=`cat /home/pi/log/$uname"_sysinfo.ftp"`
 current_sysinfo_log=`ls -ltr /home/pi/log/$uname"_sysinfo.log"`
@@ -21,20 +20,20 @@ current_access_log=`ls -ltr /home/pi/log/$uname"_access.log"`
 
 
 if [ "$prev_sysinfo_dump"  != "$current_sysinfo_dump" ]; then
-        curl -T /home/pi/log/$uname"_sysinfo.dump" -u $USER:$PASSWORD ftp://ftp.raspi.soobratty.com/
-        ls -ltr /home/pi/log/$uname"_sysinfo.dump" > $uname"_sysinfodump.ftp"
+        curl -T /home/pi/log/$uname"_sysinfodump.log" -u $USER:$PASSWORD ftp://ftp.raspi.soobratty.com/
+        ls -ltr /home/pi/log/$uname"_sysinfodump.log" > $uname"_sysinfodump.ftp"
 fi
 
 
 
 if [ "$prev_sysinfo_log"  != "$current_sysinfo_log" ]; then
-	curl -T /home/pi/log/$uname"_sysinfo.log" -u $USER:$PASSWORD ftp://ftp.raspi.soobratty.com/
-	ls -ltr /home/pi/log/$uname"_sysinfo.log" > $uname"_sysinfo.ftp"
+        curl -T /home/pi/log/$uname"_sysinfo.log" -u $USER:$PASSWORD ftp://ftp.raspi.soobratty.com/
+        ls -ltr /home/pi/log/$uname"_sysinfo.log" > $uname"_sysinfo.ftp"
 fi
 
 
 
 if [ "$prev_access_log" != "$current_access_log" ]; then
-	curl -T /home/pi/log/$uname"_access.log" -u $USER:$PASSWORD ftp://ftp.raspi.soobratty.com
-	ls -ltr /home/pi/log/$uname"_access.log" > $uname"_accesslog.ftp"
+        curl -T /home/pi/log/$uname"_access.log" -u $USER:$PASSWORD ftp://ftp.raspi.soobratty.com
+        ls -ltr /home/pi/log/$uname"_access.log" > $uname"_accesslog.ftp"
 fi
