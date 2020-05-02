41f8441b670faf2c41ffecada4b57674fded15f2
essans <30513008+essans@users.noreply.github.com>
2020-05-02 11:20:31 -0400

    Update configure_cluster.py

--- cluster/configure_cluster.py
+++ cluster/configure_cluster.py
@@ -1,65 +1,96 @@
 #!/usr/bin/env python3
 
-#useful script for executing commands across cluster with option of including the master 
-#
-#./cluster_config.py --help
-#optional arguments:
-#  -h, --help show this help message and exit
-#  -c         command to execute (default is to print hostnames)
-#  -m         include master node (Y/N)
-
 import sys
 import argparse
+
 from fabric import Connection
 
 parser = argparse.ArgumentParser(description="execute command across cluster")
 
-parser.add_argument('-c',
+parser.add_argument('-v',
         required=False,
         type=str,
-        default='echo $(hostname), $(hostname -I)',
-        help='command to execute')
+        default='Y',
+        help="Verbose (default: Y)")
+
+parser.add_argument('-c',
+	required=False,
+	type=str,
+	default='echo $(hostname), $(hostname -I)',
+	help="command to execute (default: 'hostname -I')")
 
 parser.add_argument('-m',
+	required=False,
+	type=str,
+	default='N',
+	help='include master node (default:  N)')
+
+
+parser.add_argument('-n',
         required=False,
-        type=str,
-        default='N',
-        help='include master node (Y/N)')
+        type=int,
+        default=99,
+        help='node number (default: 99 for all)')
+
 
 args = parser.parse_args()
 
+
 hosts = [
-        'xxx.xxx.xxx.xxx',
-        'xxx.xxx.xxx.xxx',
-        'xxx.xxx.xxx.xxx',
-        'xxx.xxx.xxx.xxx', 
-        'xxx.xxx.xxx.xxx' 
+        '192.168.5.41', #1
+        '192.168.5.42', #2
+        '192.168.5.19', #3
+        '192.168.5.8', #4
+        '192.168.5.9' #5
 ]
 
-master = 'xxx.xxx.xxx.xxx'
+master = '192.168.5.1'
 
-user   = "pi"
+user   = ''
 
-password = ""
-m_password = ''  #master
+password = ''
+
+m_password = ''
 
 
 if args.m in ['Y','y']:
+
+	if args.v in ['Y','y']:
+		print('\n >>> ATTEMPTING MASTER \n')
 
 	try:
 		c = Connection(master,user=user,connect_kwargs={"password": m_password})
 		result = c.run(args.c)
 
 	except:
-		print("Problem with master node")
+		print('Problem with master node \n')
 
-                
-for host in hosts:
+
+if args.n !=99:
+
+	if args.v in ['Y','y']:
+                print('\n >>> ATTEMPTING NODE: '+str(arg.n)+'\n')
 
 	try:
-		c = Connection(host,user=user,connect_kwargs={"password": password})
+		c = Connection(hosts[args.n-1],user=user,connect_kwargs={"password": password})
 		result = c.run(args.c)
 
 	except:
-		print("Problem with "+host)
-  
+		print("Problem with node "+args.n+'\n')
+
+	sys.exit()	
+
+
+
+for node,host in enumerate(hosts):
+
+	if args.v in ['Y','y']:
+                print('\n >>> ATTEMPTING NODE: '+str(node+1)+'\n')
+
+	try:
+		c = Connection(host,user=user,connect_kwargs={"password": password})
+
+		result = c.run(args.c)
+
+	except:
+		print("Problem with node "+host+'\n')
