08f1d3bdf22cb745980980fbfa19fa0d0c12e230
essans <30513008+essans@users.noreply.github.com>
2021-02-26 23:58:34 -0500

    Create pihole_log_backup.sh

---
+++ pihole/pihole_log_backup.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#sample_log=$(zcat /var/log/pihole.log.2.gz | head -n 500 | tail -n 1)
+ 
+date_log=$(stat -c '%y' /var/log/pihole.log.2.gz | cut -d ' ' -f1 | sed -e 's/-//g')
+
+date_delta=1
+
+date_yest=$(date --date="${date_log} -${date_delta} day" +%Y-%m-%d)
+
+filename="pihole.log.${date_yest}.gz"
+
+echo $filename
+
+cp /var/log/pihole.log.2.gz ~/archive/test.gz
