1e1d18e65699661ff4aa7ba4e7d248dabc6389c3
essans <30513008+essans@users.noreply.github.com>
2021-03-14 09:57:23 -0400

    Create cluster_parallel.py

---
+++ Clusters/cluster_parallel.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+import sys
+import subprocess
+
+
+cmds_to_execute =   {1:"'sudo sed -i \"s/raspberrypi/node1/g\" /etc/hostname'",   
+                     2:"'sudo sed -i \"s/raspberrypi/node2/g\" /etc/hostname'",
+                     3:"'sudo sed -i \"s/raspberrypi/node3/g\" /etc/hostname'",
+                     4:"'sudo sed -i \"s/raspberrypi/node4/g\" /etc/hostname'",
+                     5:"'sudo sed -i \"s/raspberrypi/node5/g\" /etc/hostname'"
+
+                    }
+
+for node,command in cmds_to_execute.items():
+
+        cmd_to_send = "./cluster_config.py -c " + command + " -n " +str(node)
+
+        subprocess.call(cmd_to_send, shell = True)
