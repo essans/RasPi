056751a9a0872dc73be41121ddec9cc52f9b62ec
essans <30513008+essans@users.noreply.github.com>
2021-03-14 14:25:20 -0400

    Create myconfigs.py

---
+++ Clusters/myconfigs.py
@@ -0,0 +1,28 @@
+class cluster1:
+    master_ip = '192.xxx.x.xxx'
+
+    master_pwd = 'blah'
+
+    nodes = {
+                0:'192.xxx.x.x0',
+                1:'192.xxx.x.x1',
+                2:'192.xxx.x.x2',
+                3:'192.xxx.x.x3',
+                4:'192.xxx.x.x4',
+                5:'192.xxx.x.x5'}
+
+    users = {
+                0:'user',
+                1:'user',
+                2:'user',
+                3:'user',
+                4:'user',
+                5:'user'}
+
+    passwords = {
+                0:'blah',
+                1:'blah',
+                2:'blah',
+                3:'blah',
+                4:'blah',
+                5:'blah'}
