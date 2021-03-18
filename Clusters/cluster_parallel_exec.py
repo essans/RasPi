83f392d721f6a6ba1e81d107fab99d1dd3ee14ee
essans <30513008+essans@users.noreply.github.com>
2021-03-17 23:43:18 -0400

    Update cluster_parallel_exec.py

--- Clusters/cluster_parallel_exec.py
+++ Clusters/cluster_parallel_exec.py
@@ -1,105 +1,96 @@
 #!/usr/bin/env python3
 
+#!/usr/bin/env python3
 
-import sys
 import subprocess
 import argparse
-
 from myconfigs import cluster1 as cluster
-
 from fabric import Connection
 
-
 def get_args():
-	parser = argparse.ArgumentParser(description="execute command in parallel across worker nodes")
 
-	parser.add_argument('-verbose',
-        	required=False,
-        	type=str,
-        	default='Y',
-        	help="Verbose (default: Y)")
+	parser = argparse.ArgumentParser(description="execute command across cluster")
 
-	parser.add_argument('-output',
+
+	parser.add_argument('-p','--password',
+ 		action='store_true',
+                help="use passwords from config file instead of ssh keys")
+
+
+	parser.add_argument('-l','--logging',
 	        required=False,
-	        type=str,
-	        default='Y',
-	        help="show output (default: Y)")
+	        action='store_true',
+	        help="log only instead of printing commands to screen")
 
-	parser.add_argument('-cmd',
+
+	parser.add_argument('-q','--quiet',
+	        required=False,
+	        action='store_true',
+	        help="execute quietly without showing output (default: show output)")
+
+
+	parser.add_argument('-c','--command',
 		required=False,
 		type=str,
-		default='hostname',
-		help="command to execute (default: 'hostname')")
+		default='echo $(hostname), $(hostname -I)',
+		help="command to execute (default: 'hostname -I')")
 
-	parser.add_argument('-master',
+	parser.add_argument('-m','--master',
 		required=False,
-		type=str,
-		default='N',
-		help='include master node (default:  N)')
+		action='store_true',
+		help='include execution on master node')
 
-	parser.add_argument('-nodes',
+	parser.add_argument('-n','--nodes',
 	        required=False,
-	        nargs='*',
+		nargs='*',
 	        type=int,
 	        default=99,
-	        help='node number (default: 99 for all)')
-
-	parser.add_argument('-screens',
-	        required=False,
-	        type=str,
-	        default='Y',
-	        help='screen essions (Y to create new (default), E to use existing')
-
+	        help='node numbers (default: 99 for all)')
 
 	args = parser.parse_args()
 
-	if args.output in ['Y','y']:
-		args.output = False
-
-	else:
-		args.output = True
-
 
 	return args
-
-
 
 
 def main():
 
 	args = get_args()
 
-	script = '~/code/python/cluster_exec_serial.py -v Y -o Y'
-	
+	script = '~/code/python/cluster/cluster_serial_exec.py '
+
 	screen_cmd = 'screen -S node{a} -p 0 -X stuff \'{b} -c "{c}" -n {a}\'^M'
 
+	args_to_send = '{a}{b}{c}{d}'.format(
+				a = args.command,
+				b = ' -p' if args.password else '',
+				c = ' -l' if args.logging else '',
+				d = ' -q' if args.quiet else '')
 
-	if args.screens in ['Y','y']:
-		subprocess.call('pkill screen', shell=True)
+
+	subprocess.call('pkill screen', shell=True)
 
 
 	for node,node_ip in cluster.nodes.items():
-			
-		if (node==0 and not args.master in ['Y','y'] and args.nodes==99) \
+
+		if (node==0 and not args.master and args.nodes==99) \
 			or (args.nodes!=99 and node not in args.nodes):
-			
+
 			continue
 
+		
+		subprocess.call('screen -dmS ' + 'node' + str(node), shell=True)
 
-		if args.screens in ['Y','y']:
-			subprocess.call('screen -dmS ' + 'node' + str(node), shell=True)
-
-		if args.verbose in ['Y','y']:
+		if not args.logging:
                 	print('\n >>> ATTEMPTING NODE: {}...'.format(node))
 	
-		try:
 
-			exec_cmd = screen_cmd.format(a=node, b=script, c=args.cmd)
+		try:
+			exec_cmd = screen_cmd.format(a = node, b = script, c = args_to_send)
 			subprocess.call(exec_cmd, shell=True)
 
 		except:
 			print("Problem with node "+str(node)+'\n')
-
 
 
 if __name__ == '__main__':
